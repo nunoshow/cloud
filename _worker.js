1e9ba5303c6b3722c61df248a602fcd50b687fa8
nunoshow <nuno@hotmail.nl>
2024-11-18 13:24:17 +0100

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,6 +1,14 @@
 export default {
   async fetch(request) {
     const url = new URL(request.url);
-    return fetch(url.toString());
+    return fetch(request, {
+      cf: {
+        minify: {
+          html: true,
+          css: true,
+          js: true
+        }
+      }
+    });
   }
 }

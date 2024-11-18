a3231f7b178876699b3aaf074b813aa9c649b0a7
nunoshow <nuno@hotmail.nl>
2024-11-18 12:58:35 +0100

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,12 +1,5 @@
 export default {
   async fetch(request) {
-    const url = new URL(request.url);
-    return fetch(url, {
-      headers: request.headers,
-      method: request.method,
-      body: request.body,
-      redirect: 'follow'
-    });
+    return fetch(request);
   }
 }
-

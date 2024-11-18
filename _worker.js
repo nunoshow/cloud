7668306149a5d10b6346318776ba3d33c0a6b51b
nunoshow <nuno@hotmail.nl>
2024-11-18 12:12:55 +0100

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,16 +1,9 @@
 export default {
-  async fetch(request, env) {
-    try {
-      const url = new URL(request.url);
-      const response = await fetch(request.url, {
-        method: request.method,
-        headers: request.headers,
-        body: request.body
-      });
-      return response;
-    } catch (e) {
-      return new Response(`${e.message}`, { status: 500 });
-    }
+  async fetch(request) {
+    const url = new URL(request.url);
+    return fetch(url.toString(), {
+      method: request.method,
+      headers: request.headers
+    });
   }
 }
-

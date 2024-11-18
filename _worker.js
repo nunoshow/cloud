a6c95bac7e13601cef34772bf335887fce1ab647
nunoshow <nuno@hotmail.nl>
2024-11-18 15:09:51 +0100

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,10 +1,5 @@
 export default {
   async fetch(request) {
-    const url = new URL(request.url);
-    return fetch(request, {
-      headers: {
-        'Content-Type': 'text/html'
-      }
-    });
+    return fetch(request);
   }
 }

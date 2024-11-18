f6dede09f84f6330156529fe528e3b5c301f6067
nunoshow <nuno@hotmail.nl>
2024-11-18 15:15:52 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,34 +1,13 @@
 <?php
-function neon_database_setup() {
+// Basic database connection
+function setup_neon_connection() {
     global $wpdb;
-    
-    // Optimize database connection
-    $wpdb->query("SET SESSION sql_mode = ''");
     $wpdb->query("SET NAMES 'utf8'");
-    
-    // Enable prepared statements
-    $wpdb->prepare_enable = true;
-    
-    // Set up caching
-    if (!defined('WP_CACHE')) {
-        define('WP_CACHE', true);
-    }
 }
-add_action('init', 'neon_database_setup');
+add_action('init', 'setup_neon_connection');
 
-// Performance optimizations
-function performance_optimizations() {
-    if (!is_admin()) {
-        wp_deregister_script('jquery');
-        wp_register_script('jquery', false);
-    }
+// Simple caching
+function enable_basic_cache() {
+    define('WP_CACHE', true);
 }
-add_action('wp_enqueue_scripts', 'performance_optimizations');
-
-// Security headers
-function security_headers() {
-    header('X-Frame-Options: SAMEORIGIN');
-    header('X-XSS-Protection: 1; mode=block');
-    header('X-Content-Type-Options: nosniff');
-}
-add_action('send_headers', 'security_headers');
+add_action('init', 'enable_basic_cache');

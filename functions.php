62239404b4a23356d8653b9aba671c1d5b3d5b84
nunoshow <nuno@hotmail.nl>
2024-11-18 13:27:33 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,19 @@
 <?php
-function d1_connection() {
+function neon_connection() {
     global $wpdb;
-    $wpdb->d1 = true;
+    $wpdb->neon = true;
+    
+    // Add Neon-specific optimizations
+    $wpdb->query("SET SESSION sql_mode = ''");
+    $wpdb->query("SET NAMES 'utf8'");
 }
-add_action('init', 'd1_connection');
+add_action('init', 'neon_connection');
+
+// Add caching support
+function enable_page_caching() {
+    if (!is_admin()) {
+        define('WP_CACHE', true);
+    }
+}
+add_action('init', 'enable_page_caching');
+

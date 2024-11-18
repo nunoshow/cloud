52f5fed5d95870f4917d168e404d752f826ce581
nunoshow <nuno@hotmail.nl>
2024-11-18 12:40:25 +0100

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,13 +1,24 @@
+import { Redis } from '@upstash/redis'
+
 export default {
-  async fetch(request) {
-    const url = new URL(request.url);
-    const response = await fetch(url.toString(), {
-      method: request.method,
-      headers: {
-        'Content-Type': 'text/html',
-        'Cache-Control': 'public, max-age=3600'
-      }
-    });
-    return response;
+  async fetch(request, env) {
+    const redis = new Redis({
+      url: env.UPSTASH_REDIS_REST_URL,
+      token: env.UPSTASH_REDIS_REST_TOKEN,
+    })
+
+    const url = new URL(request.url)
+    const key = `page:${url.pathname}`
+    
+    // Get page data from Redis
+    const pageData = await redis.get(key)
+    if (pageData) {
+      return new Response(pageData, {
+        headers: { 'Content-Type': 'text/html' }
+      })
+    }
+
+    return fetch(url.toString())
   }
 }
+

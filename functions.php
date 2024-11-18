d27e9241609e0b70dec619e3c36acc7684e25d97
nunoshow <nuno@hotmail.nl>
2024-11-18 13:37:31 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,19 +1,34 @@
 <?php
-function neon_connection() {
+function neon_database_setup() {
     global $wpdb;
-    $wpdb->neon = true;
     
-    // Add Neon-specific optimizations
+    // Optimize database connection
     $wpdb->query("SET SESSION sql_mode = ''");
     $wpdb->query("SET NAMES 'utf8'");
-}
-add_action('init', 'neon_connection');
-
-// Add caching support
-function enable_page_caching() {
-    if (!is_admin()) {
+    
+    // Enable prepared statements
+    $wpdb->prepare_enable = true;
+    
+    // Set up caching
+    if (!defined('WP_CACHE')) {
         define('WP_CACHE', true);
     }
 }
-add_action('init', 'enable_page_caching');
+add_action('init', 'neon_database_setup');
 
+// Performance optimizations
+function performance_optimizations() {
+    if (!is_admin()) {
+        wp_deregister_script('jquery');
+        wp_register_script('jquery', false);
+    }
+}
+add_action('wp_enqueue_scripts', 'performance_optimizations');
+
+// Security headers
+function security_headers() {
+    header('X-Frame-Options: SAMEORIGIN');
+    header('X-XSS-Protection: 1; mode=block');
+    header('X-Content-Type-Options: nosniff');
+}
+add_action('send_headers', 'security_headers');

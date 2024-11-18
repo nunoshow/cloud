90e5825c06cdd48b1c5c2d3a31159c1cc1f48d42
nunoshow <nuno@hotmail.nl>
2024-11-18 12:45:10 +0100

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,1 +1,18 @@
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
+    
+    // Direct Redis access using Cloudflare's built-in KV
+    const response = await fetch(url.toString(), {
+      method: request.method,
+      headers: {
+        'Content-Type': 'text/html',
+        'Cache-Control': 'public, max-age=3600'
+      }
+    });
+    
+    return response;
+  }
+}
+
 

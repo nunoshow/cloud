84ca4f1be1540ce6edca4c1f31b04a197a254192
nunoshow <nuno@hotmail.nl>
2024-11-18 12:49:19 +0100

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,19 +1,12 @@
 export default {
   async fetch(request) {
     const url = new URL(request.url);
-    const response = await fetch(url.toString(), {
-      cf: {
-        cacheTtl: 3600,
-        cacheEverything: true
-      }
-    });
-    
-    const content = await response.text();
-    return new Response(content, {
-      headers: {
-        'Content-Type': 'text/html',
-        'Cache-Control': 'public, max-age=3600'
-      }
+    return fetch(url, {
+      headers: request.headers,
+      method: request.method,
+      body: request.body,
+      redirect: 'follow'
     });
   }
 }
+

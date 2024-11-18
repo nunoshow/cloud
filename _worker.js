de8c83d40fa93f1db29c87b946630c2711d9ac26
nunoshow <nuno@hotmail.nl>
2024-11-18 13:56:48 +0100

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,11 +1,8 @@
 export default {
   async fetch(request) {
-    const url = new URL(request.url);
-    return fetch(request, {
-      cf: {
-        cacheEverything: true,
-        cacheTtl: 3600
-      }
+    return fetch(request.url, {
+      method: request.method,
+      headers: request.headers
     });
   }
 }
